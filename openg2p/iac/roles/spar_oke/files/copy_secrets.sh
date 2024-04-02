8a804eb8b491467a04a5198bff54b62de2ebdd3a
Deepak Devadathan <deepak.devadathan@oracle.com>
2024-04-02 01:12:07 +0000

    test
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- openg2p/iac/roles/spar_oke/files/copy_secrets.sh
+++ openg2p/iac/roles/spar_oke/files/copy_secrets.sh
@@ -5,8 +5,8 @@ function copying_secrets() {
   COPY_UTIL=./copy_cm_func.sh
   DST_NS=config-server  # DST_NS: Destination namespace
   $COPY_UTIL secret db-common-secrets postgres $DST_NS
-#   $COPY_UTIL secret keycloak keycloak $DST_NS
-#   $COPY_UTIL secret keycloak-client-secrets keycloak $DST_NS
+  $COPY_UTIL secret keycloak keycloak $DST_NS
+  $COPY_UTIL secret keycloak-client-secrets keycloak $DST_NS
 #   $COPY_UTIL secret activemq-activemq-artemis activemq $DST_NS
 #   $COPY_UTIL secret softhsm-kernel softhsm $DST_NS
 #   $COPY_UTIL secret softhsm-ida softhsm $DST_NS
