88048a91df219ed717fa8bc41171c9f3b666e4c6
Deepak Devadathan <deepak.devadathan@oracle.com>
2024-04-14 05:56:12 +0000

    test
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- sunbird_rc/iac/roles/sbrc_oke_deploy/files/init_vault.sh
+++ sunbird_rc/iac/roles/sbrc_oke_deploy/files/init_vault.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# set commands for error handling.
+set -e
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
+
 sleep 60
 kubectl -n vault exec vault-0 -- vault operator init \
 -key-shares=1 \
@@ -13,3 +20,5 @@ kubectl -n vault exec vault-0 -- /bin/sh -c "vault login $vault_root_token && va
 
 echo "vault_unseal_key: $vault_unseal_key" > /root/sunbird-rc/customization/vault/vault_secrets.yaml
 echo "vault_root_token: $vault_root_token" >> /root/sunbird-rc/customization/vault/vault_secrets.yaml
+
+echo "vault init completed" > /root/sunbird-rc/customization/vault/vault_init.done
